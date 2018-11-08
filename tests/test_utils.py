b8cf288df6dc8ecb38e3ced52a37359c3aaeb367
anastasia <anastasia.aizman@gmail.com>
2018-11-08 14:03:15 -0500

    add - tests

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+from utils import *
+
+
+def check_response(response, status_code=200):
+    assert response.status_code == status_code
+
+
+def test_get_api_url():
+    """
+    Make sure we get a functioning API URL
+    """
+    api_url = get_api_url()
+    resp = requests.get(api_url)
+    check_response(resp)
+
+    content = resp.json()
+    assert "bulk" in content.keys()
+    assert "cases" in content.keys()
