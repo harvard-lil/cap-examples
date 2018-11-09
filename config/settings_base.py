978ef810c966730a0c833272b8a2efec77826822
anastasia <anastasia.aizman@gmail.com>
2018-11-09 15:32:00 -0500

    update - get full directory path

--- config/settings_base.py
+++ config/settings_base.py
@@ -1,4 +1,6 @@
+import os
+
 API_URL = "https://api.case.law"
 API_VERSION = "v1"
 API_BULK_URL = API_URL + "/" + API_VERSION + "/" + "bulk"
-DATA_DIR = 'data'
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data')
