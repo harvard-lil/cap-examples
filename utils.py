7dc15b3a1ccfe452adf39b61f84f336125097355
anastasia <anastasia.aizman@gmail.com>
2018-11-09 15:31:34 -0500

    update - utils

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import zipfile
 import io
@@ -10,8 +11,12 @@ CEND = '\33[0m'
 CURL = '\33[4m'
 
 
-def get_api_url():
-    return "%s/%s/" % (settings.API_URL, settings.API_VERSION)
+def get_api_url(resource=None):
+    root_url = "%s/%s/" % (settings.API_URL, settings.API_VERSION)
+    if not resource:
+        return root_url
+
+    return root_url + resource + '/'
 
 
 def get_jurisdictions():
@@ -27,7 +32,7 @@ def print_info(instruction):
     print(CVIOLET + instruction + CEND)
 
 
-def get_cases(jurisdiction):
+def get_cases_from_bulk(jurisdiction):
     bulk_url = settings.API_BULK_URL + "/?body_format=text&filter_type=jurisdiction"
     bulk_api_results = requests.get(bulk_url)
     found = False
@@ -41,10 +46,27 @@ def get_cases(jurisdiction):
 
     r = requests.get(jur['download_url'])
     z = zipfile.ZipFile(io.BytesIO(r.content))
-    print_info("extracting %s into /data dir" % jur['file_name'])
+
+    print_info("extracting %s into ../data dir" % jur['file_name'])
     z.extractall(path=settings.DATA_DIR)
+    print_info("Done.")
+
+    return os.path.join(settings.DATA_DIR, jur['filename'] + '/data/data.jsonl.xz')
 
 
+def get_cases_from_api(**kwargs):
+    """
+    Get back json of the first 100 cases unless a cursor argument is provided
+    """
+    api_url = get_api_url(resource='cases')
+    filters = "?format=json&"
+    for key, val in kwargs.items():
+        filters += "%s=%s&" % (key, val)
 
-# TODO:
-# / ftp style browser: bulk/massachusetts/latest
+    api_url += filters
+
+    headers = {'AUTHORIZATION': 'Token {}'.format(settings.API_KEY)}
+    response = requests.get(api_url, headers=headers)
+
+    return response.json()
+
