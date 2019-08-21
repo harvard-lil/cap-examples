d3b68d203eee09eaf9879b28142432d10b5cd730
anastasia <anastasia.aizman@gmail.com>
2019-08-21 12:41:31 -0400

    add - help text for how to get api key

--- config/settings_base.py
+++ config/settings_base.py
@@ -1,6 +1,9 @@
 import os
 
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data')
+
 API_URL = "https://api.case.law"
 API_VERSION = "v1"
 API_BULK_URL = API_URL + "/" + API_VERSION + "/" + "bulk"
-DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data')
+# claim your key at https://case.law/user/details
+API_KEY = "123"
